5c23ea091e05774eef374a0d582b4d8c09426b33
smunoz <sgmunoz89@gmail.com>
2019-11-27 22:26:33 -0300

    creando el context

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// crear el context
+
+const CategoriasContext = React.createContext();
+
+export const CategoriasConsumer = CategoriasContext.Consumer;
+
+class categoriasProvider extends Component {
+    
+    token = '5CSA2GD5HJIXDC5M5NOY';
+
+    state = {
+        categorias : []
+    }
+
+    componentDidMount(){
+        this.obtenerCategorias();
+    }
+
+    obtenerCategorias = async () => {
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+        let categorias = await axios.get(url)
+
+        this.setState({
+            categorias : categorias.data.categories
+        })
+    }
+
+    render() {
+        return (
+            <CategoriasContext.Provider
+                value = {{
+                    categorias : this.state.categorias
+                }}
+            >
+                {this.props.children}
+            </CategoriasContext.Provider>
+        );
+    }
+}
+
+export default categoriasProvider;
